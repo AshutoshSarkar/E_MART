f78be4071764c706e412fc202ec640e09e842c40
Ashutosh Sarkar <ashut.sarkar8187@gmail.com>
2024-01-09 22:25:21 +0530

    project completed

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./DescriptionBox.css";
+const DescriptionBox = () => {
+  return (
+    <div className="descriptionbox">
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam laboriosam voluptate perferendis 
+                doloremque quasi eos optio quis tempora illo ad officiis laudantium sit libero minima, 
+                vitae nobis quos incidunt molestias ab assumenda quaerat magni velit quidem? A, aperiam consequatur
+                 accusamus unde tenetur adipisci asperiores ullam amet voluptatibus atque similique pariatur?
+                 </p>
+                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia,
+                     nostrum veritatis vero quo in eum nihil! Vel, magnam quidem. Sed, dolorem unde! Possimus veniam eveniet
+                      labore minus officia eaque expedita?</p>
+        </div>
+      </div>
+    
+  );
+};
+
+export default DescriptionBox;
--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,13 +1,82 @@
-import React, { createContext } from "react";
-import all_product from "../Components/Assets/all_product";
+import React, { createContext, useState } from "react";
+import all_product from '../Components/Assets/all_product';
+
 export const ShopContext = createContext(null);
 
-const ShopContextProvider = (props) => {
-  const contextValue = {all_product};
+const getDefaultCart = () => {
+  let cart = {};
 
-  return (<ShopContext.Provider value = {contextValue}>
-{props.children}
-  </ShopContext.Provider>)
+  for (let index = 0; index < all_product.length; index++) {
+    cart[index] = 0;
+  }
+  return cart;
+};
+
+const ShopContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState(getDefaultCart());
+
+  const addToCart = (itemId) => {
+    setCartItems((prev) => {
+      return { ...prev, [itemId]: prev[itemId] + 1 };
+    });
+  };
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => {
+      return { ...prev, [itemId]: prev[itemId] - 1 };
+    });
+  };
+
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = all_product.find((product) => product.id === Number(item));
+        totalAmount += itemInfo.new_price * cartItems[item];
+      }
+    }
+
+    return totalAmount;
+  };
+
+  const getTotalCartItems = () => {
+    let totalItem = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        totalItem += cartItems[item];
+      }
+    }
+    return totalItem;
+  };
+
+  const calculateShippingFee = () => {
+    // Assuming free shipping if the subtotal is greater than 400, else 80 rupees
+    return getTotalCartAmount() > 400 ? 0 : 80;
+  };
+
+  const placeOrder = () => {
+    // You can customize this function based on your actual order placement logic
+    // For now, we'll just show an alert
+    alert("Order Placed Successfully! Thank You!");
+  };
+
+  const contextValue = {
+    getTotalCartItems,
+    getTotalCartAmount,
+    all_product,
+    cartItems,
+    addToCart,
+    removeFromCart,
+    calculateShippingFee,
+    placeOrder,
+  };
+
+  return (
+    <ShopContext.Provider value={contextValue}>
+      {props.children}
+    </ShopContext.Provider>
+  );
 };
 
 export default ShopContextProvider;
